c968ae8f8f9a1a2add340909ecd087716fe84d07
Bagawe, Emerson <145763124+Emerson-Bagawe@users.noreply.github.com>
2023-11-08 18:36:19 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # android
 
 ### 11/2/2023
--made Product Information Section
+-made Product Information Screen
+
+### 11/8/2023
+-finalized Product Information Screen
---
+++ app/src/main/java/com/example/product_view/transaction_screen.kt
@@ -0,0 +1,11 @@
+package com.example.product_view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class transaction_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction_screen)
+    }
+}
